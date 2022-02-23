442ac52b80f3daf65462d03590af939daeadec10
dmitruk <dmitruk_89@mail.ru>
2022-02-23 17:30:28 +0300

    add structure

---
+++ my-app/src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="images/social.jpg" alt="social" />
+      </div>
+      <div>ava</div>
+      <div>
+        My posts
+        <div>new post</div>
+      </div>
+      <div>Post1</div>
+      <div>Post2</div>
+    </div>
+  );
+};
+
+export default Profile;
